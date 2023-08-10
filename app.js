22460e4235fd802738afb6e46b2c9df13c59eb0b
LauMol <63677546+Lautaromolina@users.noreply.github.com>
2023-08-10 19:03:51 -0300

    verificando js

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-	function checkScreenWidth() {
-		const element = document.querySelector(".navegacion");
-		const screenWidth = window.innerWidth;
+const button = document.querySelector(".hamburger-button");
+const menuList = document.querySelector(".menu-list");
 
-		if (screenWidth <= 578) {
-			element.classList.remove(".mobile-menu");
-		} else {
-			element.classList.add(".mobile-menu");
-		}
-	}
-
-	checkScreenWidth();
-
-	const button = document.querySelector(".hamburger-button");
-	const menuList = document.querySelector(".menu-list");
-
-	button.addEventListener("click", function () {
-		menuList.classList.toggle("active");
-	});
-
-	window.addEventListener("resize", function () {
-		checkScreenWidth();
-	});
+button.addEventListener("click", function () {
+	menuList.classList.toggle("active");
 });
